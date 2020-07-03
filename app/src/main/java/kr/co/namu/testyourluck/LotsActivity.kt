2e2f6987e6f10e997387be068c336ae5bef6f9cb
jwpark97114 <jwpark97114@gmail.com>
2020-07-03 04:05:50 -0700

    인원수 변경

--- app/src/main/java/kr/co/namu/testyourluck/LotsActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/LotsActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.testyourluck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lots.*
 
 class LotsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,22 @@ class LotsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lots)
 
         title = "제비 뽑기"
+
+        peopleCountMinusBtn.setOnClickListener {
+//            현재 몇명인지 받아오기
+            var peopleCount = peopleCountTxt.text.toString().toInt()
+
+//            사람 줄이기
+            peopleCount--
+
+//            인원수를 문구 변경
+            peopleCountTxt.text = peopleCount.toString()
+        }
+
+        peopleCountPlusBtn.setOnClickListener {
+            var peopleCount = peopleCountTxt.text.toString().toInt()
+            peopleCount ++
+            peopleCountTxt.text=peopleCount.toString()
+        }
     }
 }
