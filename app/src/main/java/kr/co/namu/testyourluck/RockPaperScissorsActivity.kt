84fec4736995f6252f43433de890a4842806eaf6
jwpark97114 <jwpark97114@gmail.com>
2020-07-03 06:11:55 -0700

    가위 바위 보 결과까지 얻는 코드

--- app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.testyourluck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_rock_paper_scissors.*
 
 class RockPaperScissorsActivity : AppCompatActivity() {
@@ -9,14 +10,17 @@ class RockPaperScissorsActivity : AppCompatActivity() {
 //    그림파일 id값 저장할 목록
     val imgList = ArrayList<Int>()
 
+//    컴퓨터가 무엇을 냈는지 저장
+    var cpuPick = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rock_paper_scissors)
 
 //        목록에 그림 세장 추가
         imgList.add(R.drawable.scissors)
-        imgList.add(R.drawable.paper)
         imgList.add(R.drawable.rock)
+        imgList.add(R.drawable.paper)
 
 
         scissorBtn.setOnClickListener {
@@ -28,6 +32,10 @@ class RockPaperScissorsActivity : AppCompatActivity() {
 
 //            랜덤으로 뽑힌 그림 띄워주기
             cpuImg.setImageResource(thisTimePick)
+
+            cpuPick = randomNum
+            getResultOfRPS(myPick = 0)
+
 
         }
 
@@ -41,6 +49,10 @@ class RockPaperScissorsActivity : AppCompatActivity() {
 //            랜덤으로 뽑힌 그림 띄워주기
             cpuImg.setImageResource(thisTimePick)
 
+
+            cpuPick = randomNum
+            getResultOfRPS(myPick = 1)
+
         }
 
         paperBtn.setOnClickListener {
@@ -53,7 +65,52 @@ class RockPaperScissorsActivity : AppCompatActivity() {
 //            랜덤으로 뽑힌 그림 띄워주기
             cpuImg.setImageResource(thisTimePick)
 
+
+            cpuPick = randomNum
+            getResultOfRPS(myPick = 2)
+
         }
 
+    }
+
+//    승패판정
+    fun getResultOfRPS(myPick:Int){
+
+//    사용자가 가위를 냈다면
+        if(myPick == 0){
+            if(cpuPick==0){
+                Toast.makeText(this,"무승부",Toast.LENGTH_SHORT).show()
+            }
+            else if(cpuPick==1){
+                Toast.makeText(this,"패배",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"승리",Toast.LENGTH_SHORT).show()
+            }
+        }
+        else if(myPick == 1){
+            if(cpuPick==0){
+                Toast.makeText(this,"승리",Toast.LENGTH_SHORT).show()
+            }
+            else if(cpuPick==1){
+                Toast.makeText(this,"무승부",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"패배",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        else{
+            if(cpuPick==0){
+                Toast.makeText(this,"패배",Toast.LENGTH_SHORT).show()
+            }
+            else if(cpuPick==1){
+                Toast.makeText(this,"승리",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"무승부",Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
