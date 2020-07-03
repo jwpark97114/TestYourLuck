61684b3a3b3722dba155df69bc924a65ccc8504a
jwpark97114 <jwpark97114@gmail.com>
2020-07-03 06:01:09 -0700

    그림파일 랜덤 출력

--- app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
@@ -5,13 +5,29 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_rock_paper_scissors.*
 
 class RockPaperScissorsActivity : AppCompatActivity() {
+
+//    그림파일 id값 저장할 목록
+    val imgList = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rock_paper_scissors)
 
+//        목록에 그림 세장 추가
+        imgList.add(R.drawable.scissors)
+        imgList.add(R.drawable.paper)
+        imgList.add(R.drawable.rock)
+
+
         scissorBtn.setOnClickListener {
 
-            cpuImg.setImageResource(R.drawable.scissors)
+//            0부터 2까지 하나를 랜덤으로 추출 ()
+            val randomNum = (Math.random() * 3).toInt()
+
+            val thisTimePick = imgList[randomNum]
+
+//            랜덤으로 뽑힌 그림 띄워주기
+            cpuImg.setImageResource(thisTimePick)
 
         }
 
