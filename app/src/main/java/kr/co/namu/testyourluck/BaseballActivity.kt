cc0efda986768a419e81045467b732da0b4be5ae
jwpark97114 <jwpark97114@gmail.com>
2020-07-04 02:46:02 -0700

    컴퓨터의 숫자야구 문제 출제 기능

--- app/src/main/java/kr/co/namu/testyourluck/BaseballActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/BaseballActivity.kt
@@ -2,11 +2,15 @@ package kr.co.namu.testyourluck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_baseball.*
 import kr.co.namu.testyourluck.adapters.ChattingAdapter
 import kr.co.namu.testyourluck.datas.ChattingMessage
 
 class BaseballActivity : AppCompatActivity() {
+
+//    컴퓨터가 문제로 낸 세자리 숫자 저장할 배열
+    val cpuNumbers = ArrayList<Int>()
 
 
 //    Chatting log between a cpu and a user.
@@ -28,7 +32,48 @@ class BaseballActivity : AppCompatActivity() {
         mChatAdapter = ChattingAdapter(this,R.layout.chatting_message_list_item,chattingMessageList)
         chattingListView.adapter = mChatAdapter
 
+//        문제 출제 기능 실행
+        makeCpuNumbers()
 
 
     }
+
+//    컴퓨터가 문제를 내주는 기능
+
+    fun makeCpuNumbers(){
+//        cpuNumbers에 랜덤한 세자리 숫자 배치
+        for (i in 0..2){
+
+//            중복이면 숫자 다시 뽑아야한다 => 중복이 아닐때 까지 계속 뽑는다
+            while (true){
+//                1~9 정수 랜덤 뽑기
+                val randomNum = (Math.random()*9+1).toInt()
+
+//              이미 뽑은 숫자인지 중복검사
+                var isDuplicatedNum = false
+
+//                문제로 나온 숫자를 돌려봐서 같은 숫자가 발견되면 중복인걸로 처리
+
+                for(num in cpuNumbers){
+                    if(num == randomNum){
+                        isDuplicatedNum = true
+                    }
+                }
+
+//                중복이 아니어야 숫자 사용가능
+                if(!isDuplicatedNum){
+                    cpuNumbers.add(randomNum)
+                    break
+                }
+            }
+
+        }
+
+//        로그로 문제 숫자 화인
+        for(num in cpuNumbers){
+            Log.d("문제출제",num.toString())
+        }
+    }
+
+
 }
